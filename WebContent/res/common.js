1dafc045bcfd08fdae76a9dc96685a418f026c1f
Cweili <3weili@gmail.com>
2013-05-10 10:00:08 +0800

    Fix.

--- WebContent/res/common.js
+++ WebContent/res/common.js
@@ -1,61 +1,60 @@
-var record = new Record();
-
 /**
  * 错题记录
  * 
  * @returns
  */
-function Record() {
-	this.userCookieName = 'driving-test-user';
-	
-	this.getUser = function() {
+var record = {
+	userCookieName : 'driving-test-user',
+
+	getUser : function() {
 		var cookie = Ext.util.Cookies.get(this.userCookieName);
 		if (null == cookie || '' == cookie) {
 			cookie = '' + parseInt(Math.random() * 2147483647);
-			Ext.util.Cookies.set(this.userCookieName, cookie, new Date((new Date()).getTime() + 365 * 24 * 3600 * 1000));
+			Ext.util.Cookies.set(this.userCookieName, cookie, new Date((new Date()).getTime() + 365
+					* 24 * 3600 * 1000));
 			return cookie;
 		}
 		return cookie;
-	};
-	
-	this.addWrongQuestion = function(review, id) {
+	},
+
+	addWrongQuestion : function(review, id) {
 		recordService.addWrongQuestion(this.getUser(), review, id);
-	};
-	
-	this.getWrongQuestion = function(review, callback) {
+	},
+
+	getWrongQuestion : function(review, callback) {
 		recordService.getWrongQuestion(this.getUser(), review, function(data) {
 			callback(data);
 		});
-	};
-	
-	this.removeWrongQuestion = function(review, id, callback) {
+	},
+
+	removeWrongQuestion : function(review, id, callback) {
 		recordService.removeWrongQuestion(this.getUser(), review, id, function(data) {
 			callback(data);
 		});
-	};
+	}
 };
 
 var messageBox = function(title, msg, target) {
 	if (target) {
-		Ext.MessageBox.show({  
-			title: title,
-			msg: msg,
-			width: 400,
-			closable: true,
-			buttons: Ext.MessageBox.OK,
-			icon: Ext.MessageBox.INFO,
-			animateTarget: target 
+		Ext.MessageBox.show({
+			title : title,
+			msg : msg,
+			width : 400,
+			closable : true,
+			buttons : Ext.MessageBox.OK,
+			icon : Ext.MessageBox.INFO,
+			animateTarget : target
 		});
 		setTimeout(function() {
 			Ext.MessageBox.hide();
 		}, 1200);
 	} else {
-		Ext.MessageBox.show({  
-			title: title,  
-			msg: msg,  
-			width: 400, 
-			closable: false,  
-			icon: Ext.MessageBox.INFO
+		Ext.MessageBox.show({
+			title : title,
+			msg : msg,
+			width : 400,
+			closable : false,
+			icon : Ext.MessageBox.INFO
 		});
 	}
 };
